2221c95236bad5518d4ba98d99a3215de107832e
PetarPaskas <petar.paskas@hotmai.com>
2021-08-16 04:43:24 +0200

    Implemented IRepository

--- Marketshop/Core/Repositories/IRepository.cs
+++ Marketshop/Core/Repositories/IRepository.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace Marketshop.Core.Repositories
 {
-    interface IRepository
+    interface IRepository<TEntity> where TEntity : class
     {
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+
+        IEnumerable<TEntity> Find(Expression<Func<TEntity,bool>> predicate);
     }
 }
