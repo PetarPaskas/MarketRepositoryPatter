f5ae19c53b12deb71c98431d7a0f31739d51281a
PetarPaskas <petar.paskas@hotmai.com>
2021-08-17 03:29:09 +0200

    KupacRepositoryInitialized

--- Marketshop/Persistence/Repositories/KupacRepository.cs
+++ Marketshop/Persistence/Repositories/KupacRepository.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Marketshop.Core.Domain;
+using System.Data.Entity;
+using Marketshop.Core.Repositories;
 
 namespace Marketshop.Persistence.Repositories
 {
-    class KupacRepository
+    class KupacRepository : Repository<Kupac>, IKupacRepository
     {
+        protected MarketContext Context { get { return _context as MarketContext; } }
+
+        public KupacRepository(MarketContext context)
+        :base(context)
+        {
+        }
+
+        public IEnumerable<Kupac> KupciSaNjihovimKupovinama()
+        {
+            return Context.Kupci.Include(k => k.Kupovine).ToList();
+        }
+
+       
+        public IEnumerable<Kupac> KupciSaNajviseBodova(int pageSize = 5, int pageIndex = 1)
+        {
+            return Context.Kupci.ToList().OrderByDescending(k => k.Bodovi).Skip((pageIndex-1)*pageSize).Take(pageSize);
+        }
     }
 }
